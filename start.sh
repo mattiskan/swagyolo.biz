9f801fe7b289707fbe4f2d95b6fd54892018cb45
mattiksan <server@mattiksan.se>
2014-05-22 10:44:24 +0200

    added start script

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#remove previously running server                                               
+if [ -f nodeserver.pid ]
+then
+    id=`cat nodeserver.pid`
+
+    if ps -p $id > /dev/null
+    then
+        kill $id
+    fi
+fi
+
+#start server
+cmd='npm start &>node.log & echo $! > nodeserver.pid'
+echo $cmd | at now 2> /dev/null
+
+echo -n "Starting node server..."
+sleep 1 #give time for server to start
+
+
+if ps -p $(cat nodeserver.pid) > /dev/null
+then
+    echo 'Done.'
+else
+    cat node.log
+fi
